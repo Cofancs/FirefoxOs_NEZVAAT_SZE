b76c9d88fa00966f9b73b938793b320f956efd8f
Cofancs <Cofancs@DESKTOP-LRNVKIT>
2015-12-01 19:50:36 +0100

    Made a lot of changes.

--- js/app.js
+++ js/app.js
@@ -1,29 +1,26 @@
-// DOMContentLoaded is fired once the document has been loaded and parsed,
-// but without waiting for other external resources to load (css/images/etc)
-// That makes the app more responsive and perceived as faster.
-// https://developer.mozilla.org/Web/Reference/Events/DOMContentLoaded
-window.addEventListener('DOMContentLoaded', function() {
-
-  // We'll ask the browser to use strict code to help us catch errors earlier.
-  // https://developer.mozilla.org/Web/JavaScript/Reference/Functions_and_function_scope/Strict_mode
-  'use strict';
-
-  var translate = navigator.mozL10n.get;
-
-  // We want to wait until the localisations library has loaded all the strings.
-  // So we'll tell it to let us know once it's ready.
-  navigator.mozL10n.once(start);
-
-  // ---
-
-  function start() {
-
-    var message = document.getElementById('message');
-
-    // We're using textContent because inserting content from external sources into your page using innerHTML can be dangerous.
-    // https://developer.mozilla.org/Web/API/Element.innerHTML#Security_considerations
-    message.textContent = translate('message');
-
-  }
-
-});
+var href= window.content.location.href.substring( window.location.href.lastIndexOf( '-' ));
+console.log(href);
+var walesi=document.getElementById("walesi");
+var anyam=document.getElementById("anyam");
+var tiszai=document.getElementById("tiszai");
+var title = document.getElementById("title");
+var author = document.getElementById("author");
+if(href == "-walesi"){
+    title.textContent="A walesi bárdok";
+    author.textContent="Arany János";
+    walesi.hidden=false;
+    anyam.hidden=true;
+    tiszai.hidden=true;
+}else if(href == "-anyam"){
+        title.textContent="Anyám tyúkja";
+    author.textContent="Petőfi Sándor";
+    walesi.hidden=true;
+    anyam.hidden=false;
+tiszai.hidden=true;
+}else if(href == "-tiszai"){
+        title.textContent="Tiszai Csönd";
+    author.textContent="Juhász Gyula";
+    walesi.hidden=true;
+    anyam.hidden=true;
+    tiszai.hidden=false;
+}
