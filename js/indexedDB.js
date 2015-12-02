03f07040e27fda022de70b481111a0d837a5bbf5
Cofancs <Cofancs@DESKTOP-LRNVKIT>
2015-12-02 11:09:47 +0100

    Made changes at indeyedDB

--- js/indexedDB.js
+++ js/indexedDB.js
@@ -1,7 +1,5 @@
 
-window.indexedDB = window.indexedDB;
-window.IDBTransaction = window.IDBTransaction;
-window.IDBKeyRange = window.IDBKeyRange;
+
 if (!window.indexedDB) {
     window.alert("Your browser doesn't support a stable version of IndexedDB. Such and such feature will not be available.");
 }
@@ -225,322 +223,311 @@ var poemData = [
         youtube_url: "https://www.youtube.com/watch?v=FxOZvSlIT3M"
     }
 ];
-
-var dbName = "poems_db";
-var dbVersion = 3;
-var dbStoreName = "poems";
+var dbName = "poems4_db";
+var dbVersion = 1;
+var dbStoreName = "poems4";
 function addPoemLine(poem) {
     var $li = document.createElement("li");
     $li.innerHTML = "" + poem.title + "<br />" + poem.author;
     document.getElementById("poems").appendChild($li);
 }
-var db=window.indexedDB;
-var request = db.open(dbName,dbVersion);
 
-request.onupgradeneeded = function(event){
-  db=event.currentTarget.result;
-  var objectStore = db.createObjectStore(dbStoreName,{keyPath: "id", autoIncrement: true});
-  objectStore.createIndex('id','id',{unique: true});
-  objectStore.createIndex('title', 'title', { unique: false });
-  objectStore.createIndex('author', 'author', { unique: false });
-  
-  
-};
+var pDb = window.indexedDB;
+var request = pDb.open(dbName, dbVersion);
+request.onupgradeneeded = function (event) {
+    console.log("onupgraded start");
+    var db = event.currentTarget.result;
+    /*if(db.objectStoreNames.contains(dbStoreName)) { 
+     db.objectStoreName.delete();
+     }*/
 
-request.onsuccess = function(){
-    db = this.result;
-    
-    var transaction = db.transaction(dbStoreName,"readonly");
-    
-    transaction.oncomplete = function(){
+    var objectStore = db.createObjectStore(dbStoreName, {keyPath: 'id'});
+    objectStore.createIndex("id", "id", {unique: true});
+    console.log("middle start");
+    var transaction = db.transaction(dbStoreName, "readwrite");
+    var poem;
+    transaction.oncomplete = function () {
+        for (var i in poemData) {
+            poem = poemData[i];
+            addPoemLine(poem);
+        }
+
         db.close();
+        console.log("transaction end");
     };
-    var objectStore= transaction.objectStore(dbStoreName);
-    
-    var req = objectStore.openCursor();
-    req.onsuccess = function (event){
-      var cursor = event.target.result;
-      
-      if(cursor){
-          addPoemLine(""+cursor.value.title + "<br />" + cursor.value.author +"");
-          cursor.continue();
-      }else{
-          console.log("end");
-      }
-    };
-    req.onerror = function(){
-        console.log("error opening the cursor");
-    };
+    var objectStoreTrans = transaction.objectStore(dbStoreName);
+    for (var i in poemData) {
+        poem = poemData[i];
+        var req = objectStoreTrans.add(poem);
+        req.onsuccess = function () {
+            console.log("Success");
+        };
+        req.onerror = function () {
+            console.log("Error objectTrans");
+        };
+    }
+    console.log("onupgraded end");
 };
-
-function addPoem(){
-    var db = window.indexedDB;
-    
-    var request = db.open(dbName,dbVersion);
-    
-    request.onsuccess = function(){
-      db=this.result;
-      var transaction= db.transaction(dbStoreName,"readwrite");
-      var poem;
-      transaction.oncomplete = function(){
-          for (var i in poemData) {
-                         poem=poemData[i];
-                         addPoemLine(poem);
-          }
-        
-        db.close();
-      };
-      var objectStore = transaction.objectStore(dbStoreName);
-      
-      for (var i in poemData) {
-                         poem=poemData[i];
-                        
-                         
-                         var req = objectStore.add(poem);
-                         req.onsuccess = function(){
-                             console.log("Success");
-                         };
-                         req.onerror= function(){
-                             console.log("Error");
-                         };
-          }
-      
-    };
-    request.onerror = function(){
-        console.log("Error");
-    };
-}
-
+request.onsuccess = function () {
+    /* var db = this.result;
+     console.log("onsuccess start");
+     var transaction = db.transaction(dbStoreName,"readonly");
+     
+     transaction.oncomplete = function(){
+     console.log("oncomplete start");
+     db.close();
+     };
+     var objectStore= transaction.objectStore(dbStoreName);
+     
+     var req = objectStore.openCursor();
+     req.onsuccess = function (event){
+     var cursor = event.target.result;
+     
+     if(cursor){
+     addPoemLine(""+cursor.value.title + "<br />" + cursor.value.author +"");
+     cursor.continue();
+     }else{
+     console.log("end");
+     }
+     };
+     req.onerror = function(){
+     console.log("error opening the cursor");
+     };*/
+    console.log("onsuccess end");
+};
+request.onerror = function () {
+    console.log("Error on request");
+};
 /*(function () {
-  
-
-
-var db;
-
-// Used to keep track of which view is displayed to avoid uselessly reloading it
-  var current_view_poem_key;
-  
-function openDb() {
-console.log("openDb ...");
-        var request = indexedDB.open(dbName, dbVersion);
-        request.onsuccess = function (event) {
-        db = this.result;
-                console.log("openDb DONE");
-                request.onerror = function (event) {
-                console.log("Database error: " + event.target.errorCode);
-                };
-                request.onupgradeneeded = function (event) {
-                console.log("openDb.onupgradeneeded");
-                        var store = event.currentTarget.result.createObjectStore(dbStoreName, {keyPath: "id", autoIncrement: true});
-                        store.createIndex('title', 'title', { unique: false });
-                        store.createIndex('author', 'author', { unique: false });
-                        /*objectStore.createIndex("id", "id", {unique: true});
-                         
-                         objectStore.transaction.oncomplete = function (event) {
-                         // Store values in the newly created objectStore.
-                         var poemObjectStore = db.transaction(dbStoreName, "readwrite").objectStore("poems");
-                         for (var i in poemData) {
-                         poemObjectStore.add(poemData[i]);
-                         poemObjectStore.onsucces = function (event) {
-                         
-                         }
-                         }
-                         };
-                         };
-                };
-        }*/
+ 
+ 
+ 
+ var db;
+ 
+ // Used to keep track of which view is displayed to avoid uselessly reloading it
+ var current_view_poem_key;
+ 
+ function openDb() {
+ console.log("openDb ...");
+ var request = indexedDB.open(dbName, dbVersion);
+ request.onsuccess = function (event) {
+ db = this.result;
+ console.log("openDb DONE");
+ request.onerror = function (event) {
+ console.log("Database error: " + event.target.errorCode);
+ };
+ request.onupgradeneeded = function (event) {
+ console.log("openDb.onupgradeneeded");
+ var store = event.currentTarget.result.createObjectStore(dbStoreName, {keyPath: "id", autoIncrement: true});
+ store.createIndex('title', 'title', { unique: false });
+ store.createIndex('author', 'author', { unique: false });
+ /*objectStore.createIndex("id", "id", {unique: true});
+ 
+ objectStore.transaction.oncomplete = function (event) {
+ // Store values in the newly created objectStore.
+ var poemObjectStore = db.transaction(dbStoreName, "readwrite").objectStore("poems");
+ for (var i in poemData) {
+ poemObjectStore.add(poemData[i]);
+ poemObjectStore.onsucces = function (event) {
+ 
+ }
+ }
+ };
+ };
+ };
+ }*/
 /**
  * @param {string} store_name
  * @param {string} mode either "readonly" or "readwrite"
  */
 /*function getObjectStore(store_name, mode) {
-var tx = db.transaction(store_name, mode);
-        return tx.objectStore(store_name);
-}
-function clearObjectStore(store_name) {
-var store = getObjectStore(dbName, 'readwrite');
-        var req = store.clear();
-        req.onsuccess = function(evt) {
-        console.log("Store cleared");
-                displayPoemList(store);
-        };
-        req.onerror = function (evt) {
-        console.error("clearObjectStore:", evt.target.errorCode);
-                console.log(this.error);
-        };
-}
-function getBlob(key, store, success_callback) {
-var req = store.get(key);
-        req.onsuccess = function(evt) {
-        var value = evt.target.result;
-                if (value)
-                success_callback(value.blob);
-        };
-}*/
+ var tx = db.transaction(store_name, mode);
+ return tx.objectStore(store_name);
+ }
+ function clearObjectStore(store_name) {
+ var store = getObjectStore(dbName, 'readwrite');
+ var req = store.clear();
+ req.onsuccess = function(evt) {
+ console.log("Store cleared");
+ displayPoemList(store);
+ };
+ req.onerror = function (evt) {
+ console.error("clearObjectStore:", evt.target.errorCode);
+ console.log(this.error);
+ };
+ }
+ function getBlob(key, store, success_callback) {
+ var req = store.get(key);
+ req.onsuccess = function(evt) {
+ var value = evt.target.result;
+ if (value)
+ success_callback(value.blob);
+ };
+ }*/
 /**
  * @param {IDBObjectStore=} store
  */
 /*function displayPoemList(store) {
-console.log("displayPoemList");
-        if (typeof store == 'undefined')
-        store = getObjectStore(dbName, 'readonly');
-        var poem_msg = $('#poem-msg');
-        poem_msg.empty();
-        var poem_list = $('#poem-list');
-        poem_list.empty();
-        // Resetting the iframe so that it doesn't display previous content
-        newViewerFrame();
-        var req;
-        req = store.count();
-        // Requests are executed in the order in which they were made against the
-        // transaction, and their results are returned in the same order.
-        // Thus the count text below will be displayed before the actual pub list
-        // (not that it is algorithmically important in this case).
-        req.onsuccess = function(evt) {
-        poem_msg.append('<p>There are <strong>' + evt.target.result +
-                '</strong> record(s) in the object store.</p>');
-        };
-        req.onerror = function(evt) {
-        console.error("add error", this.error);
-                console.log(this.error);
-        };
-        var i = 0;
-        req = store.openCursor();
-        req.onsuccess = function(evt) {
-        var cursor = evt.target.result;
-                // If the cursor is pointing at something, ask for the data
-                if (cursor) {
-        console.log("displayPoemList cursor:", cursor);
-                req = store.get(cursor.key);
-                req.onsuccess = function (evt) {
-                var value = evt.target.result;
-                        var list_item = $('<li>' +
-                                value.title + "<br />"+
-                                value.author +
-                                '</li>');
-                           if (value.hasOwnProperty('blob') &&
-                                typeof value.blob != 'undefined') {
-                var link = $('<a href="' + cursor.key + '">File</a>');
-                        link.on('click', function() { return false; });
-                        link.on('mouseenter', function(evt) {
-                        setInViewer(evt.target.getAttribute('href')); });
-                        list_item.append(' / ');
-                        list_item.append(link);
-                } else {
-                list_item.append(" / No attached file");
-                }
-                poem_list.append(list_item);
-                };
-                // Move on to the next object in store
-                cursor.continue();
-                // This counter serves only to create distinct ids
-                i++;
-        } else {
-        console.log("No more entries");
-        }
-        };
-}
-function newViewerFrame() {
-    var viewer = $('#poem-viewer');
-    viewer.empty();
-    var iframe = $('<iframe />');
-    viewer.append(iframe);
-    return iframe;
-  }
-
-  function setInViewer(key) {
-    console.log("setInViewer:", arguments);
-    key = Number(key);
-    if (key == current_view_poem_key)
-      return;
-
-    current_view_poem_key = key;
-
-    var store = getObjectStore(dbStoreName, 'readonly');
-    getBlob(key, store, function(blob) {
-      console.log("setInViewer blob:", blob);
-      var iframe = newViewerFrame();
-
-      // It is not possible to set a direct link to the
-      // blob to provide a mean to directly download it.
-      if (blob.type == 'text/html') {
-        var reader = new FileReader();
-        reader.onload = (function(evt) {
-          var html = evt.target.result;
-          iframe.load(function() {
-            $(this).contents().find('html').html(html);
-          });
-        });
-        reader.readAsText(blob);
-      } else if (blob.type.indexOf('image/') == 0) {
-        iframe.load(function() {
-          var img_id = 'image-' + key;
-          var img = $('<img id="' + img_id + '"/>');
-          $(this).contents().find('body').html(img);
-          var obj_url = window.URL.createObjectURL(blob);
-          $(this).contents().find('#' + img_id).attr('src', obj_url);
-          window.URL.revokeObjectURL(obj_url);
-        });
-      } else if (blob.type == 'application/pdf') {
-        $('*').css('cursor', 'wait');
-        var obj_url = window.URL.createObjectURL(blob);
-        iframe.load(function() {
-          $('*').css('cursor', 'auto');
-        });
-        iframe.attr('src', obj_url);
-        window.URL.revokeObjectURL(obj_url);
-      } else {
-        iframe.load(function() {
-          $(this).contents().find('body').html("No view available");
-        });
-      }
-
-    });
-  }
-
-  */
-  /**
-   * @param {string} biblioid
-   * @param {string} title
-   * @param {number} year
-   * @param {Blob=} blob
-   */
-  /*function addPoem(id, title, author, blob) {
-    console.log("addPublication arguments:", arguments);
-    var obj = { id: biblioid, title: title, author: author };
-    if (typeof blob != 'undefined')
-      obj.blob = blob;
-
-    var store = getObjectStore(dbStoreName, 'readwrite');
-    var req;
-    try {
-      req = store.add(obj);
-    } catch (e) {
-      if (e.name == 'DataCloneError')
-        console.log("This engine doesn't know how to clone a Blob, " +
-                             "use Firefox");
-      throw e;
-    }
-    req.onsuccess = function (evt) {
-      console.log("Insertion in DB successful");
-      console.log();
-      displayPubList(store);
-    };
-    req.onerror = function() {
-      console.error("addPublication error", this.error);
-      console.log(this.error);
-    };
-  }
-
-  
-
-
-  
+ console.log("displayPoemList");
+ if (typeof store == 'undefined')
+ store = getObjectStore(dbName, 'readonly');
+ var poem_msg = $('#poem-msg');
+ poem_msg.empty();
+ var poem_list = $('#poem-list');
+ poem_list.empty();
+ // Resetting the iframe so that it doesn't display previous content
+ newViewerFrame();
+ var req;
+ req = store.count();
+ // Requests are executed in the order in which they were made against the
+ // transaction, and their results are returned in the same order.
+ // Thus the count text below will be displayed before the actual pub list
+ // (not that it is algorithmically important in this case).
+ req.onsuccess = function(evt) {
+ poem_msg.append('<p>There are <strong>' + evt.target.result +
+ '</strong> record(s) in the object store.</p>');
+ };
+ req.onerror = function(evt) {
+ console.error("add error", this.error);
+ console.log(this.error);
+ };
+ var i = 0;
+ req = store.openCursor();
+ req.onsuccess = function(evt) {
+ var cursor = evt.target.result;
+ // If the cursor is pointing at something, ask for the data
+ if (cursor) {
+ console.log("displayPoemList cursor:", cursor);
+ req = store.get(cursor.key);
+ req.onsuccess = function (evt) {
+ var value = evt.target.result;
+ var list_item = $('<li>' +
+ value.title + "<br />"+
+ value.author +
+ '</li>');
+ if (value.hasOwnProperty('blob') &&
+ typeof value.blob != 'undefined') {
+ var link = $('<a href="' + cursor.key + '">File</a>');
+ link.on('click', function() { return false; });
+ link.on('mouseenter', function(evt) {
+ setInViewer(evt.target.getAttribute('href')); });
+ list_item.append(' / ');
+ list_item.append(link);
+ } else {
+ list_item.append(" / No attached file");
+ }
+ poem_list.append(list_item);
+ };
+ // Move on to the next object in store
+ cursor.continue();
+ // This counter serves only to create distinct ids
+ i++;
+ } else {
+ console.log("No more entries");
+ }
+ };
+ }
+ function newViewerFrame() {
+ var viewer = $('#poem-viewer');
+ viewer.empty();
+ var iframe = $('<iframe />');
+ viewer.append(iframe);
+ return iframe;
+ }
  
-
-  openDb();
-  addEventListeners();
-
-}})(); // Immediately-Invoked Function Expression (IIFE)
-*/
+ function setInViewer(key) {
+ console.log("setInViewer:", arguments);
+ key = Number(key);
+ if (key == current_view_poem_key)
+ return;
+ 
+ current_view_poem_key = key;
+ 
+ var store = getObjectStore(dbStoreName, 'readonly');
+ getBlob(key, store, function(blob) {
+ console.log("setInViewer blob:", blob);
+ var iframe = newViewerFrame();
+ 
+ // It is not possible to set a direct link to the
+ // blob to provide a mean to directly download it.
+ if (blob.type == 'text/html') {
+ var reader = new FileReader();
+ reader.onload = (function(evt) {
+ var html = evt.target.result;
+ iframe.load(function() {
+ $(this).contents().find('html').html(html);
+ });
+ });
+ reader.readAsText(blob);
+ } else if (blob.type.indexOf('image/') == 0) {
+ iframe.load(function() {
+ var img_id = 'image-' + key;
+ var img = $('<img id="' + img_id + '"/>');
+ $(this).contents().find('body').html(img);
+ var obj_url = window.URL.createObjectURL(blob);
+ $(this).contents().find('#' + img_id).attr('src', obj_url);
+ window.URL.revokeObjectURL(obj_url);
+ });
+ } else if (blob.type == 'application/pdf') {
+ $('*').css('cursor', 'wait');
+ var obj_url = window.URL.createObjectURL(blob);
+ iframe.load(function() {
+ $('*').css('cursor', 'auto');
+ });
+ iframe.attr('src', obj_url);
+ window.URL.revokeObjectURL(obj_url);
+ } else {
+ iframe.load(function() {
+ $(this).contents().find('body').html("No view available");
+ });
+ }
+ 
+ });
+ }
+ 
+ */
+/**
+ * @param {string} biblioid
+ * @param {string} title
+ * @param {number} year
+ * @param {Blob=} blob
+ */
+/*function addPoem(id, title, author, blob) {
+ console.log("addPublication arguments:", arguments);
+ var obj = { id: biblioid, title: title, author: author };
+ if (typeof blob != 'undefined')
+ obj.blob = blob;
+ 
+ var store = getObjectStore(dbStoreName, 'readwrite');
+ var req;
+ try {
+ req = store.add(obj);
+ } catch (e) {
+ if (e.name == 'DataCloneError')
+ console.log("This engine doesn't know how to clone a Blob, " +
+ "use Firefox");
+ throw e;
+ }
+ req.onsuccess = function (evt) {
+ console.log("Insertion in DB successful");
+ console.log();
+ displayPubList(store);
+ };
+ req.onerror = function() {
+ console.error("addPublication error", this.error);
+ console.log(this.error);
+ };
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ openDb();
+ addEventListeners();
+ 
+ }})(); // Immediately-Invoked Function Expression (IIFE)
+ */
 
